a40cd39d28efc2eb95b452ab4916b42d8655dfff
viktor0980 <viktor0980@gmail.com>
2023-08-07 12:36:38 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blog from './pages/blog';
+import SignUp from './pages/signup';
+  
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/events' component={Events} />
+        <Route path='/annual' component={AnnualReport} />
+        <Route path='/team' component={Teams} />
+        <Route path='/blog' component={Blog} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
+  );
+}
+  
+export default App;
